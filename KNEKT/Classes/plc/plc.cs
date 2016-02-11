b8a673aab9ff416edd8877d6f1fdc43a9e090360
Hanrich <Hanrichp@gmail.com>
2016-02-11 17:43:47 +0200

    working on creating a singelton interfacte

---
+++ KNEKT/Classes/plc/plc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KNEKT.Classes.plc
+{
+    public sealed class Plc
+    {
+        private static readonly Lazy<Plc> lazy =
+        new Lazy<Plc>(() => new Plc());
+        public static Plc Instance { get { return lazy.Value; } }
+        private Plc()
+        {
+        }
+
+    }
+}
