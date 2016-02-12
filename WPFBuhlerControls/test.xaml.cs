4cc5298fb806e3f6ec306501d2a148e4c4c24ce0
Hanrich <Hanrichp@gmail.com>
2016-02-12 08:21:37 +0200

    PLC Communication getting there

--- WPFBuhlerControls/test.xaml.cs
+++ WPFBuhlerControls/test.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Snap7;
+using System.Threading;
 
 
 namespace WPFBuhlerControls
@@ -21,22 +22,52 @@ namespace WPFBuhlerControls
     /// </summary>
     public partial class test : UserControl
     {
+        
         public String name { get; set; }
+        S7Client plc;
+
+        public class Worker
+        {
+            S7Client plc;
+            public Worker(S7Client tmp) {
+                plc = tmp;
+            }
+            // This method will be called when the thread is started.
+            public void DoWork()
+            {
+                while (!_shouldStop)
+                {
+                    if (plc.Connected())
+                    {
+                        //Console.Out.WriteLine("Connected");
+                    }
+                    else
+                    {
+                        //Console.Out.WriteLine("Not Connected");
+                    }
+                }
+                
+            }
+            public void RequestStop()
+            {
+                _shouldStop = true;
+            }
+            // Volatile is used as hint to the compiler that this data
+            // member will be accessed by multiple threads.
+            private volatile bool _shouldStop;
+        }
 
         public test()
         {
             InitializeComponent();
-            name = "hanrich";
-            S7Client test = Plc.Instance;
-            if (test.Connected())
-            {
-                Console.WriteLine("Control is connected");
-            }
-            else
-            {
-                Console.WriteLine("no connection");
-            }
+            plc = Plc.Instance;
+            Worker workerObject = new Worker(plc);
+            Thread workerThread = new Thread(workerObject.DoWork);
+            // Start the worker thread.
+            workerThread.Start();
+
         }
+
 
     }
 }
