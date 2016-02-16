1fd663f9824bcfd2cec54f795028c43393b90038
JLHenery <jameshenery111@gmail.com>
2016-02-16 15:03:32 +0200

    Worker added to sepatator.

--- WPFBuhlerControls/Separator_MTRC.xaml.cs
+++ WPFBuhlerControls/Separator_MTRC.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WPFBuhlerControls.FB_Code;
 using System.ComponentModel;
+using Snap7;
+using System.Threading;
 
 namespace WPFBuhlerControls
 {
@@ -33,8 +35,62 @@ namespace WPFBuhlerControls
             InitializeComponent();
         }
 
+        // The worker threads runs in the background and updates our control
+        #region[Worker Thread]
+        public class Worker
+        {
+            S7Client plc;
+            Scale_Dosing1 parent;
+            private int updateTime = 100;
+            public int dbnumber { get; set; }
+            public int dboffset { get; set; }
+            public int dboffsetSpeedMonitor { get; set; }
 
+            public Worker(S7Client tmp, Scale_Dosing1 parent)
+            {
+                plc = tmp;
+                this.parent = parent;
+            }
+            // This method will be called when the thread is started.
+            public void DoWork()
+            {
+                while (!_shouldStop)
+                {
+                    if (plc != null)
+                    {
+                        if (plc.Connected())
+                        {
+                            byte[] buffer = new byte[2];
+                            Plc.DBRead(dbnumber, dboffset, 2, buffer);
+                            Array.Reverse(buffer);
+                            //Console.Out.WriteLine(BitConverter.ToUInt16(buffer, 0));
+                            parent.ScaleColor = BitConverter.ToUInt16(buffer, 0);
+                            Thread.Sleep(200);
+                        }
+                    }
+                }
+                Thread.Sleep(updateTime);
+            }
+            public void RequestStop()
+            {
+                _shouldStop = true;
+            }
+            // Volatile is used as hint to the compiler that this data
+            // member will be accessed by multiple threads.
+            private volatile bool _shouldStop;
+        }
+        #endregion
 
+        public Separator_MTRC()
+        {
+            InitializeComponent();
+            if (System.ComponentModel.LicenseManager.UsageMode == System.ComponentModel.LicenseUsageMode.Runtime)
+            {
+                workerObject = new Worker(Plc.Instance, this);
+                Thread workerThread = new Thread(workerObject.DoWork);
+                workerThread.Start();
+            }
+        }
         //------------------------------------------------------------------------------//
         //                                  Properties                                  //
         //------------------------------------------------------------------------------//
