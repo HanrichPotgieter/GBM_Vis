acf8cd7d67fe8d4a47e129d2143195c57026bd56
JLHenery <jameshenery111@gmail.com>
2016-02-25 11:52:48 +0200

    ChainConveyorState Worker Thread.

--- WPFBuhlerControls/Conveyor_Chain_AHKA_S.xaml.cs
+++ WPFBuhlerControls/Conveyor_Chain_AHKA_S.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WPFBuhlerControls.FB_Code;
 using System.ComponentModel;
+using Snap7;
+using System.Threading;
 
 namespace WPFBuhlerControls
 {
@@ -28,10 +30,65 @@ namespace WPFBuhlerControls
         private bool _MotorOnLeft = false;
         private string _ObjectNo;
         private string _PLCName;
+        Worker workerObject;
+
+        // The worker threads runs in the background and updates our control
+        #region[Worker Thread]
+        public class Worker
+        {
+            S7Client plc;
+            Conveyor_Chain_AHKA_S parent;
+            private int updateTime = 100;
+            public int dbnumber { get; set; }
+            public int dboffset { get; set; }
+
+            public Worker(S7Client tmp, Conveyor_Chain_AHKA_S parent)
+            {
+                plc = tmp;
+                this.parent = parent;
+            }
+            // This method will be called when the thread is started.
+            public void DoWork()
+            {
+                while (!_shouldStop)
+                {
+                    if (plc != null)
+                    {
+                        if (plc.Connected())
+                        {
+                            byte[] buffer = new byte[2];
+                            Plc.DBRead(dbnumber, dboffset, 2, buffer);
+                            Array.Reverse(buffer);
+                            //Console.Out.WriteLine(BitConverter.ToUInt16(buffer, 0));
+                            parent.MotorColor = BitConverter.ToUInt16(buffer, 0);
+                            Thread.Sleep(200);
+                        }
+                    }
+                }
+                Thread.Sleep(updateTime);
+            }
+            public void RequestStop()
+            {
+                _shouldStop = true;
+            }
+            // Volatile is used as hint to the compiler that this data
+            // member will be accessed by multiple threads.
+            private volatile bool _shouldStop;
+        }
+        #endregion
 
         public Conveyor_Chain_AHKA_S()
         {
             InitializeComponent();
+            if (System.ComponentModel.LicenseManager.UsageMode == System.ComponentModel.LicenseUsageMode.Runtime)
+            {
+                workerObject = new Worker(Plc.Instance, this);
+                Thread workerThread = new Thread(workerObject.DoWork);
+                workerThread.Start();
+            }
+
+           //this.MouseLeftButtonUp += new MouseButtonEventHandler(_myCustomUserControl_MouseLeftButtonUp);
+
         }
 
         //------------------------------------------------------------------------------//
